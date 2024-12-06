b33bdb1f45f147249510b6ed112533b24553b6f1
Jyldyz <35424198+jy1dyz@users.noreply.github.com>
2024-12-06 21:33:48 +0600

    refactor

--- app/src/main/java/kg/study/ksptestapp/base/ui_form/CoilImage.kt
+++ app/src/main/java/kg/study/ksptestapp/base/ui_form/CoilImage.kt
@@ -1,9 +1,14 @@
 package kg.study.ksptestapp.base.ui_form
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.compose.rememberAsyncImagePainter
@@ -13,7 +18,7 @@ fun CoilImage(url: String) {
     Image(
         painter = rememberAsyncImagePainter(url), // Image URL
         contentDescription = "downloaded Image",
-        modifier = Modifier.size(128.dp) // Adjust size as needed
+        modifier = Modifier.size(128.dp).clip(shape = RoundedCornerShape(16.dp)) // Adjust size as needed
     )
 }
 
